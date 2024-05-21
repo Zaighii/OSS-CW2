b510457a98fd832f2d93f3376d1dc8e793174918
Zaigham Khan <22109123@bucks.ac.uk>
2024-05-21 14:00:06 +0100

    navigation improved

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -3,6 +3,7 @@
 
    <a href="modules.php">My Modules</a> |
    <a href="students.php">Student</a> |
+   <a href="addstudent.php">Add Student</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="logout.php">Logout</a>
